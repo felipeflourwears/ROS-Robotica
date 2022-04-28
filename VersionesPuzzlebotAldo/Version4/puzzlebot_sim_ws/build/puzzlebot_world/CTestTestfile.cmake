c4809be6d9c6dbe17beb90b130ac1381a0dba3e2
felipeflourwears <A01274880@itesm.mx>
2022-04-27 22:22:11 -0500

    Versiones ROS puzzlebot

---
+++ VersionesPuzzlebotAldo/Version4/puzzlebot_sim_ws/build/puzzlebot_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/luisfe/Documents/gitAldo/TE3002B_CSF/Robots_Diferenciales/puzzlebot_sim_ws/src/puzzlebot_world
+# Build directory: /home/luisfe/Documents/gitAldo/TE3002B_CSF/Robots_Diferenciales/puzzlebot_sim_ws/build/puzzlebot_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ VersionesPuzzlebotAldo/Version4/puzzlebot_sim_ws/devel/share/common-lisp/ros/puzzlebot_msgs/msg/_package_GoToPoseResult.lisp
@@ -0,0 +1,4 @@
+(cl:in-package puzzlebot_msgs-msg)
+(cl:export '(SUCCESS-VAL
+          SUCCESS
+))
